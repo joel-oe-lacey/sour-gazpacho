c86f255a50c34c711e1d987eb9c9b912bb1b1d76
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-19 16:48:17 -0700

    Add user login reducer

---
+++ src/reducers/user.js
@@ -0,0 +1,8 @@
+export const userReducer = (state = {}, action) => {
+    switch (action.type) {
+        case 'LOGIN_USER':
+            return {...action.user}
+        default:
+            return state;
+    }
+}
