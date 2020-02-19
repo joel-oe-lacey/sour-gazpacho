68e227b5dbc50970da65f0474b254dc1a8b2f57d
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-18 18:03:32 -0700

    Add connect and fetching to trigger dispatch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import './App.scss';
 import Nav from '../Nav/Nav';
 import LoadingPage from '../LoadingPage/LoadingPage';
+import Homepage from '../Homepage/Homepage';
+import { connect } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import { loadMovies } from '../../actions';
+// { !this.state.movies.length && <LoadingPage /> }
+// { this.state.movies.length && <Homepage /> }
 
-export default class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-
-    }
+class App extends Component {
+  componentDidMount() {
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v1/movies')
+      .then(response => response.json())
+      .then(movieData => {
+        this.props.loadMoviesToStore(movieData.movies)
+      })
   }
 
   render() {
@@ -20,3 +27,13 @@ export default class App extends Component {
     )
   }
 }
+
+const mapDispatchToProps = (dispatch) => ({
+  loadMoviesToStore: (movies) => { dispatch(loadMovies(movies)) }
+});
+
+const mapStateToProps = state => ({
+  movies: state.movies,
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
