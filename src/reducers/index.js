b4150642f2643e4d89fda4edba19997bb9286666
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-19 16:48:29 -0700

    Add user login reducer to combineReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { moviesReducer } from './movies';
+import { userReducer } from './user';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
     movies: moviesReducer,
+    user: userReducer
 })
 
 export default rootReducer;
