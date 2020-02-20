a6ec9ab6f2ad37bd0ac19b31e853f8940330961b
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-02-20 14:11:34 -0700

    Refactor to incude loading screen

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import App from './App';
+// import { shallow } from 'enzyme'
+//
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// describe('App', () => {
+//
+//
+//   it('should match a snapshot as expected', () => {
+//     const wrapper = shallow(<App />)
+//
+//     expect(wrapper).tomMatchSnapshot()
+//   })
+// })
