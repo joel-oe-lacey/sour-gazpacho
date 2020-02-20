54f8c3dd1c13fadf1fe82c4f9f735a7072c9bf68
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-20 15:31:33 -0700

    Fix user store bug

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,7 +1,7 @@
 export const userReducer = (state = {}, action) => {
     switch (action.type) {
         case 'LOGIN_USER':
-            return {...action.user}
+            return action.user;
         default:
             return state;
     }
