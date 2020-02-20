721cb429fd342737a612e508e1f0a523a832f34c
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-20 15:31:57 -0700

    Validate user via post request

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { loginUser } from '../../actions';
 import './LoginForm.scss'
 
+//need to edit to create a post request on login
+//if successful proceed and post via dispatch 
+//if unsuccessful need to display error and prompt for retry 
+//how to display loading state in between?
+
+//set up tests for login also. 
 
 class LoginForm extends Component {
     constructor() {
         super()
         this.state = {
             email: '',
-            password: ''
+            password: '',
+            error: '',
+            auth: false
         }
     }
 
@@ -18,36 +26,60 @@ class LoginForm extends Component {
         this.setState({ [e.target.name]: e.target.value });
     }
 
+    validateUser = (e, email, password) => {
+        e.preventDefault();
+
+        const options = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password }),
+        }
+
+        fetch('https://rancid-tomatillos.herokuapp.com/api/v1/login', options)
+            .then(response => response.json())
+            .then(data => {
+                data.error && this.setState({error: data.error})
+                if(data.user) {
+                    this.props.addUserToStore(data.user) 
+                    this.setState({ auth: true })
+                }
+            })
+    }
+
     render() {
-        return (
-            <section className="form-container"> 
-                <form className="form">
-                    <h1>Please Login</h1>
-                    <input
-                        type="text"
-                        name="email"
-                        placeholder="Email..."
-                        onChange={this.handleChange}
-                        required
-                    />
-                    <input
-                        type="text"
-                        name="password"
-                        placeholder="Password..."
-                        onChange={this.handleChange}
-                        required
-                    />
-                    <Link to="/"><button
-                        onClick={() => this.props.addUserToStore({
-                            email: this.state.email,
-                            password: this.state.password
-                        })}
-                    >
-                        Login
-                    </button></Link>
-                </form>
-            </section>
-        )
+        if(this.state.auth) {
+            return <Redirect to="/" />
+        } else {
+            return (
+                <section className="form-container"> 
+                    <form className="form">
+                        <h1>Please Login</h1>
+                        <input
+                            type="text"
+                            name="email"
+                            placeholder="Email..."
+                            onChange={this.handleChange}
+                            required
+                        />
+                        <input
+                            type="text"
+                            name="password"
+                            placeholder="Password..."
+                            onChange={this.handleChange}
+                            required
+                        />
+                        <h4>{`${this.state.error}`}</h4>
+                        <button
+                            onClick={(e) => this.validateUser(e, this.state.email, this.state.password)}
+                        >
+                            Login
+                        </button>
+                    </form>
+                </section>
+            )
+        }
     }
 }
 
