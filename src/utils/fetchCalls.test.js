d3959f47af3734fccdf21e6432acd39a72b00aa4
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-02-20 15:31:36 -0700

    Initial commit

---
+++ src/utils/fetchCalls.test.js
@@ -0,0 +1,30 @@
+import { fetchData } from './fetchCalls';
+
+describe('FetchData', () => {
+
+
+  beforeEach(() => {
+    window.fetch = jest.fn(() => {
+      return Promise.resolve({
+        status: 200,
+        ok: true
+      })
+    })
+  })
+
+  it('', async () => {
+    let response = await fetchData('https://rancid-tomatillos.herokuapp.com/api/v1/movies')
+    expect(response).toEqual({ status: 200, ok: true })
+  })
+
+  it('should return an erro when unsuccessful', async () => {
+    let error = new Error('There was an error fetching data')
+    window.fetch = jest.fn(() => {
+      return Promise.resolve({
+        status: 200,
+        ok: false
+      })
+    })
+    await expect(fetchData('https://rancid-tomatillos.herokuapp.com/api/v1/movies')).rejects.toEqual(error)
+  })
+})
