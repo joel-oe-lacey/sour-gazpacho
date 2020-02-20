a0958c6a12c72df727b54258ba3e37004c624a23
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-20 16:48:19 -0700

    Add login linking to nav button

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Nav.scss';
+import { Link } from 'react-router-dom';
 import bloodSplatter from '../../assets/blood-splatter.png';
 
 const Nav = () => {
     // need to conditionally render buttons based on route
     return (
         <nav className="nav">
-            <button>Login</button>
+            <Link to="/login"><button>Login</button></Link>
             <h1>Sour Gazpacho</h1>
             <img src={bloodSplatter} alt='kill bill style blood splatter' id='blood-splatter'/>
         </nav>
