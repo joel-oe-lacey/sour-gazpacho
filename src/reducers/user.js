a8a6fffec5364d7b92f78c2655d20106f3213442
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-20 18:08:28 -0700

    Add log out reducer case

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,6 +1,9 @@
 export const userReducer = (state = {}, action) => {
+    console.log('hit', action)
     switch (action.type) {
         case 'LOGIN_USER':
+            return action.user;
+        case 'LOG_OUT':
             return action.user;
         default:
             return state;
