954c7345d64846933e8b943c1575efb0dd02cf38
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-20 18:08:52 -0700

    Include conditional rendering and log out involkation

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import './Nav.scss';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logOut } from '../../actions';
 import bloodSplatter from '../../assets/blood-splatter.png';
 
-const Nav = () => {
-    // need to conditionally render buttons based on route
+const Nav = ({ user, logOutFromStore }) => {
     return (
         <nav className="nav">
-            <Link to="/login"><button>Login</button></Link>
+            {
+                user.id ? <Link to="/"><button onClick={logOutFromStore}>Log Out</button></Link> : <Link to="/login"><button>Login</button></Link>
+            }
             <h1>Sour Gazpacho</h1>
             <img src={bloodSplatter} alt='kill bill style blood splatter' id='blood-splatter'/>
         </nav>
     )
 }
 
-export default Nav;
+const mapStateToProps = state => ({
+    user: state.user
+})
+
+const mapDispatchToProps = dispatch => ({
+    logOutFromStore: () => { dispatch(logOut()) }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Nav);
