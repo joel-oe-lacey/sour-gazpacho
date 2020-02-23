06756b6b3478517c1907f4a1df0ead30824ea4da
Virginia Ladd <virginialadd2@gmail.com>
2020-02-23 10:55:55 -0700

    Snapshot test update

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -11,9 +11,10 @@ const MovieCard = ({ id, title, userRating, averageRating, poster }) => {
           <Link to={`/movies/${id}`}><img src={poster} alt='movie poster' className='movie-poster'/></Link>
           <div className='movie-info'>
             <h3 className='movie-title'>{title}</h3>
-            {userRating && <h4 className='movie-rating'>Your Rating: {averageRating}</h4>}
             <h4 className='movie-rating'>AverageRating: {averageRating}</h4>
+            {userRating && <h4 className='movie-rating'>Your Rating: {averageRating}</h4>}
           </div>
+        </article>
     )
 }
 
