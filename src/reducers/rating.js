c4d0eb15784894f1c156f280e25cae59560495bb
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-23 10:49:46 -0700

    Setup base add rating reducer

--- src/reducers/rating.js
+++ src/reducers/rating.js
@@ -2,6 +2,8 @@ export const ratingReducer = (state = [], action) => {
     switch (action.type) {
         case 'LOAD_RATINGS':
             return action.ratings;
+        case 'ADD_RATING':
+            return [...state, action.rating]
         default:
             return state;
     }
