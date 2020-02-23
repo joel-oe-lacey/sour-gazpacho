83a78dd0e25ed7c2ec27e21281972eb3fa919661
Virginia Ladd <virginialadd2@gmail.com>
2020-02-23 13:31:20 -0700

    Finishes up Nav testing - pretty sure

--- src/components/Nav/Nav.test.js
+++ src/components/Nav/Nav.test.js
@@ -1,17 +1,37 @@
 import { Nav, mapStateToProps, mapDispatchToProps } from './Nav.js';
-import { logOut }
+import { logOut } from '../../actions';
+import { shallow } from 'enzyme';
+import React from 'react';
+
+
 
 describe('Nav', () => {
+  let mockUser = {name: 'Karl the Llama', id: 9, password: 'password1'}
+
   describe('Nav Component', () => {
-    // component testing we already know
+    let wrapper;
+    let mockLogOutFromStore = jest.fn()
+
+    beforeEach(() => {
+    wrapper=shallow(<Nav
+        user={mockUser}
+        logOutFromStore={mockLogOutFromStore}
+      />);
+    })
+
+    it('should be an instance of Nav', () => {
+      expect(wrapper).toMatchSnapshot();
+    })
+    
+
   })
   describe('mapStateToProps', () => {
     it('should return an object with an array of todos', () => {
       const mockState = {
-        user: {name: 'Karl the Llama', id: 9, password: 'password1'},
+        user: mockUser,
         movies: 'Big Lebowski'
       }
-      const expected = { user: {name: 'Karl the Llama', id: 9, password: 'password1'} }
+      const expected = { user: mockUser }
       const mappedProps = mapStateToProps(mockState)
       expect(mappedProps).toEqual(expected)
     })
@@ -22,7 +42,7 @@ describe('Nav', () => {
       const mockDispatch = jest.fn();
       const actionToDispatch = logOut();
       const dispatchedProps = mapDispatchToProps( mockDispatch );
-      dispatchedProps.logOut();
+      dispatchedProps.logOutFromStore();
       expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
     })
   })
