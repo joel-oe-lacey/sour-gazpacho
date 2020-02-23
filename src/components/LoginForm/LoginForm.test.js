a90366834ec25fc4adddd829ab07396bcdaf12fb
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-23 10:49:34 -0700

    Add initial loginForm test skeleton

---
+++ src/components/LoginForm/LoginForm.test.js
@@ -0,0 +1,18 @@
+//need to test local state changing 
+//validate user should hit fetch
+//need to mock out fetch
+//need to jest fn preventDefault
+
+//need a snapshot test
+//test mapDispatchToProps
+
+describe('LoginForm', () => {
+    describe('LoginForm Container', () => {
+        //unit tests we already know how to write
+    });
+
+    describe('mapDispatchToProps', () => {
+        //unit tests we already know how to write
+    });
+
+})
