24362f12b246a3753cd3c08b61995958f89efa84
Virginia Ladd <virginialadd2@gmail.com>
2020-02-23 23:51:24 -0700

    Updated ternary to render a button that is hidden instead of the route not going anywhere

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -64,6 +64,7 @@ export class MovieDetails extends Component{
       }
     }
     const mapRating = ratings.find(rating => rating.movie_id === movie.id)
+    console.log(movie);
     return (
       <section className="movie-details" style={ sectionStyle }>
         <h1 className='movie-title-detail'>{movie.title}</h1>
@@ -94,9 +95,9 @@ export class MovieDetails extends Component{
           <p className='detail-summary'>Overview: {movie.overview}</p>
         </div>
        <div className='detail-btns'>
-        <Link className='movie-btn' to={`/movies/${movie.id === 21 ? movie.id : movie.id-1}`}>See Previous Movie</Link>
-        <Link className='movie-btn' to='/'>Go Back to All Movies</Link>
-        <Link className='movie-btn' to={`/movies/${movie.id === 35 ? movie.id : movie.id+1}`}>See Next Movie</Link>
+        {movie.id === 21 ? <Link className='movie-btn hidden'></Link>  : <Link className='movie-btn' to={`/movies/${movie.id-1}`}>See Previous Movie</Link>}
+        <Link className='movie-btn' id='mid-btn' to='/'>Go Back to All Movies</Link>
+        {movie.id === 40 ? <Link className='movie-btn hidden'></Link> : <Link className='movie-btn' to={`/movies/${movie.id === 35 ? movie.id : movie.id+1}`}>See Next Movie</Link>}
       </div>
       </section>
     )
