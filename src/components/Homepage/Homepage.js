9b01941be36e99cc5d6f6bb07c2b3154a92ec04c
Virginia Ladd <virginialadd2@gmail.com>
2020-02-25 10:46:10 -0700

    Link CenterMovie to HomePage and grab a a random movie

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './Homepage.scss';
-import MovieCard from '../MovieCard/MovieCard'
+import MovieCard from '../MovieCard/MovieCard';
+import CenterMovie from '../CenterMovie/CenterMovie';
 
 
 const Homepage = ({ allMovies }) => {
   let movieData =  allMovies.map(movie => {
     return <MovieCard key={movie.id} title={movie.title} averageRating={movie.average_rating} poster={movie.poster_path} id={movie.id} />
   })
-
+  let randomNum = Math.floor(Math.random() * 20)
+  let centerMovie = <CenterMovie movie={allMovies[randomNum]}/>
     return (
         <section className="homepage">
-          <h1 className='movie-list-title'>Movies by Title</h1>
+          <div className='movie-centerpiece'>{centerMovie}</div>
+          <h1 className='movie-list-title'>All Movies</h1>
           <div className='movie-homepage'>{movieData}</div>
         </section>
     )
