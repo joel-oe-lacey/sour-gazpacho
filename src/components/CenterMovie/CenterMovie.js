06c5b6fb526e4afdaa5538dd711409ed1df603d5
Virginia Ladd <virginialadd2@gmail.com>
2020-02-25 10:44:52 -0700

    Create CenterMovie component

---
+++ src/components/CenterMovie/CenterMovie.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './CenterMovie.scss';
+import { Link } from 'react-router-dom';
+
+
+const CenterMovie = ({ movie }) => {
+      </div>
+export default CenterMovie
