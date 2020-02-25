0fd7e5e808bb81875cb32c696a4f9b67110806e7
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-02-25 14:50:47 -0700

    Fix most linter errors

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import { App, mapStateToProps, mapDispatchToProps } from './App';
 import { shallow } from 'enzyme'
-import { fetchData } from '../../utils/fetchCalls';
 import { loadMovies } from '../../actions';
 
 
@@ -16,38 +14,16 @@ describe('App', () => {
     })
 
     it('should call fetchData when component mounts with the correct url', () => {
-
-      const movies = {
-        movies: [
-        {
-          "id": 21,
-          "title": "Sonic the Hedgehog",
-          "poster_path": "https://image.tmdb.org/t/p/original//aQvJ5WPzZgYVDrxLX4R6cLJCEaQ.jpg",
-          "backdrop_path": "https://image.tmdb.org/t/p/original//tCUcf3oNWMW8kwAj3WC6CvIN5ah.jpg",
-          "release_date": "2020-02-12",
-          "overview": "Based on the global blockbuster videogame franchise from Sega, Sonic the Hedgehog tells the story of the world’s speediest hedgehog as he embraces his new home on Earth. In this live-action adventure comedy, Sonic and his new best friend team up to defend the planet from the evil genius Dr. Robotnik and his plans for world domination.",
-          "average_rating": 7
-        },
-        {
-          "id": 22,
-          "title": "Parasite",
-          "poster_path": "https://image.tmdb.org/t/p/original//7IiTTgloJzvGI1TAYymCfbfl3vT.jpg",
-          "backdrop_path": "https://image.tmdb.org/t/p/original//TU9NIjwzjoKPwQHoHshkFcQUCG.jpg",
-          "release_date": "2019-05-30",
-          "overview": "All unemployed, Ki-taek's family takes peculiar interest in the wealthy and glamorous Parks for their livelihood until they get entangled in an unexpected incident.",
-          "average_rating": 4
-        }
-      ]}
       window.fetch = jest.fn().mockImplementation(() => {
         return Promise.resolve({
           ok: true,
-          json: () => Promise.resolve(mockResponse)
+          json: () => Promise.resolve({})
         });
       })
       let mockFetchData = jest.fn()
       const instance = wrapper.instance();
       instance.componentDidMount = jest.fn().mockImplementation(() => mockFetchData())
-      let spy = jest.spyOn(instance, 'componentDidMount')
+      jest.spyOn(instance, 'componentDidMount')
       instance.componentDidMount();
       expect(mockFetchData).toHaveBeenCalled();
     })
@@ -140,8 +116,7 @@ describe('App', () => {
       ]
       const actionToDispatch = loadMovies(movies)
       const mappedProps = mapDispatchToProps(mockDispatch)
-      const result = mappedProps.loadMoviesToStore(movies)
-
+      mappedProps.loadMoviesToStore(movies)
       expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
     })
   })
